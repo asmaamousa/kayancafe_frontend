1c848747d8ad2f74fcfd7f96c4cde816ba1c2eed
asmaa mousa <asmaamousa978@gmail.com>
2024-07-12 22:24:32 +0300

    front end

---
+++ New folder/task 3.c
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num_inarrival;
+    cin>>num_intarrival;
+   int inarrival[num_intarrival];
+    for(int i=0;i<num_intarrival;i++){
+
+        cin>>inarrival[i];
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
